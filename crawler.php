4d8c1fa1b626abbe758e68bddc93fd290ced1a8b
john <trd-johnalex@gmail.com>
2015-11-04 17:26:28 +0800

    >>no major revision >>new class & functions added for inserting data to database >>run&test no error found

--- crawler.php
+++ crawler.php
@@ -4,8 +4,8 @@ require_once('dbconnect.php');
 
 header('Content-Type: text/html; charset=utf-8');
 
-class cURL
-{
+class cURL{
+
     public $response;
     protected $sendHeader;
     protected $PostFields;
@@ -49,7 +49,9 @@ class cURL
 
         if(!empty($this->sendHeader))
 
-            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56');
+            curl_setopt($ch, CURLOPT_USERAGENT,
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/
+                601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56');
 
         $decode = curl_exec($ch);
         $_response = ($deJSON)? json_decode($decode, true) : $decode;
@@ -77,6 +79,66 @@ class cURL
         return implode("&",$string);
     }
 }
+
+class dbFunctions
+{
+    function dbRowInsert($table_name, $form_data, $conn)
+    {
+        $fields = array_keys($form_data);
+
+        $sql = "INSERT INTO ".$table_name."
+    (`".implode('`,`', $fields)."`)
+    VALUES('".implode("','", $form_data)."')";
+
+        return mysqli_query($conn, $sql);
+    }
+
+    function dbRowDelete($table_name, $where_clause='')
+    {
+        $whereSQL = '';
+        if(!empty($where_clause))
+        {
+            if(substr(strtoupper(trim($where_clause)), 0, 5) != 'WHERE')
+            {
+                $whereSQL = " WHERE ".$where_clause;
+            } else
+            {
+                $whereSQL = " ".trim($where_clause);
+            }
+        }
+        $sql = "DELETE FROM ".$table_name.$whereSQL;
+
+        return mysql_query($sql);
+    }
+
+    function dbRowUpdate($table_name, $form_data, $where_clause='')
+    {
+        $whereSQL = '';
+        if(!empty($where_clause))
+        {
+            if(substr(strtoupper(trim($where_clause)), 0, 5) != 'WHERE')
+            {
+                $whereSQL = " WHERE ".$where_clause;
+            } else
+            {
+                $whereSQL = " ".trim($where_clause);
+            }
+        }
+        $sql = "UPDATE ".$table_name." SET ";
+
+        $sets = array();
+        foreach($form_data as $column => $value)
+        {
+            $sets[] = "`".$column."` = '".$value."'";
+        }
+        $sql .= implode(', ', $sets);
+
+        $sql .= $whereSQL;
+
+        return mysql_query($sql);
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -91,7 +153,11 @@ class cURL
 
 //$path   =   'https://www.airbnb.com/s/Fukuoka-Prefecture--Japan?guests=&checkin=11%2F02%2F2015&checkout=11%2F07%2F2015&ss_id=3qfx8hqk&source=bb';
 $path = $_GET["url"];
+if(empty($path)){
+    echo "No data found or Missing url!";
+}
 $cURL   =   new cURL();
+$dbFunc = new dbFunctions();
 $html   =   $cURL->emulateBrowser()->connect($path,false);
 $dom    =   new DOMDocument;
 @$dom->loadHTML($html);
@@ -102,13 +168,13 @@ $data = array();
 foreach($articles as $container) {
 
     foreach($container->getElementsByTagName('div') as $div) {
-        if($div->parentNode->getAttribute('class') == "col-sm-12 row-space-2 col-md-6")
-        {
+        if($div->parentNode->getAttribute('class')
+            == "col-sm-12 row-space-2 col-md-6"){
             $data["data-lat"] = $div->getAttribute('data-lat');
             $data["data-lng"] = $div->getAttribute('data-lng');
             $data["data-url"] = $div->getAttribute('data-url');
             $data["data-user"] = $div->getAttribute('data-user');
-            $data["data-id"] = $div->getAttribute('data-id');
+            $data["data-id"] = $div->getAttribute('data-id');//same value in hosting id
             $data["data-instant-book"] = $div->getAttribute('data-instant-book');
         }
     }
@@ -127,39 +193,28 @@ foreach($articles as $container) {
             $data["data-star_rating"] = $span->getAttribute('data-star_rating');
         }
     }
-    //print_r($data);echo "<br><br>";} /*
-    print_r("Description : " .$data['data-name']. "<br>");
-    print_r("Location : " .$data['data-address']. "<br>");
-    print_r("Price : " .$data['data-price']. "<br>");
-    print_r("Reviews : " .$data['data-review_count']. "<br>");
-    print_r("Rating : " .$data['data-star_rating']. "<br><br>");
+
+    echo "User ID :" .$data['data-user']. "<br>";
+    echo "Hosting ID :" .$data['data-hosting_id']. "<br>";
+    echo "Description : " .$data['data-name']. "<br>";
+    echo "Location : " .$data['data-address']. "<br>";
+    echo "Price : " .$data['data-price']. "<br>";
+    echo "Reviews : " .$data['data-review_count']. "<br>";
+    echo "Rating : " .$data['data-star_rating']. "<br><br>";
+
+    $form_data = array(
+        'id' => $data['data-id'],
+        'user' => $data['data-user'],
+        'name' => $data['data-name'],
+        'address' => $data['data-address'],
+        'price' => $data['data-price'],
+        'reviewcount' => $data['data-review_count'],
+        'rating' => $data['data-star_rating'],
+    );
+
+    $dbFunc->dbRowInsert('t_sample', $form_data, $conn);
 }
-
-/*
-    $ins = 'INSERT INTO t_sample(id, user, name, address, price, reviewcount, rating)
-     VALUES("'.$data["data-id"].'", "'.$data["data-user"].'", "'
-        .$data["data-name"].'", "'.$data["data-address"].'", "'
-        .$data["data-price"].'", "'.$data["data-review_count"].'", "'
-        .$data["data-star_rating"].'")';
-
-     if(mysqli_query($conn, $ins)){
-        echo "New record created successfully <br>";
-     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-    
-
-}
-
-
-/*
-foreach ($dom->getElementsByTagName('div') as $tag) {
-    if ($tag->getAttribute('class') === 'col-sm-12 row-space-2 col-md-6') {
-        echo $tag->nodeValue;
-    }
-}
-*/
-
 ?>
+
 </body>
 </html>
